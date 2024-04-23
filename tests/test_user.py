53164f7beb45fe185cb308fddaf2e172a2a59c8c
klawik-j <kklawik345@gmail.com>
2024-04-23 08:48:13 +0200

    Add delete user and measurement API.

--- tests/test_user.py
+++ tests/test_user.py
@@ -40,3 +40,13 @@ def test_read_user():
     response = client.get("/api/users/2")
     assert response.status_code == 200
     assert response.json() == expected_data
+
+
+def test_delete_user():
+    expected_data = {
+        "name": "John Doe2",
+        "user_id": 2,
+    }
+    response = client.delete("/api/users/2")
+    assert response.status_code == 200
+    assert response.json() == expected_data
